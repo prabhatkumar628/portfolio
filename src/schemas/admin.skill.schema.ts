b2191e317ae7ffb46429a94cbed611fb9f348a65
Prabha Kumar <kprabhat628@gmail.com>
2026-02-23 17:56:42 +0530

    fix in admin skill schema

--- src/schemas/admin.skill.schema.ts
+++ src/schemas/admin.skill.schema.ts
@@ -3,7 +3,7 @@
 import { z } from "zod";
 
 const mediaFileSchema = z.object({
-  url: z.string().optional().or(z.literal("")),      // ✅ Allow empty string
+  url: z.string().optional().or(z.literal("")), // ✅ Allow empty string
   public_id: z.string().optional().or(z.literal("")), // ✅ Allow empty string
 });
 
@@ -12,9 +12,10 @@ export const skillCreateSchema = z.object({
     .string()
     .min(2, "Skill must be at least 2 characters")
     .max(20, "Skill must be no more than 20 characters"),
-  emoji: z
-    .string()
-    .refine((val) => [...val].length === 1, "Only one emoji allowed"),
+  emoji: z.string().refine((val) => {
+    const segmenter = new Intl.Segmenter("en", { granularity: "grapheme" });
+    return [...segmenter.segment(val)].length === 1;
+  }, "Only one emoji allowed"),
   image: mediaFileSchema.optional(), // ✅ Whole object optional
   isTop: z.boolean(),
   category: z.enum(["frontend", "backend", "tools"]),
